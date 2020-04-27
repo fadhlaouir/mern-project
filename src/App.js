493364fd39d28fe1d6d687035f2ff6bfc5e7668c
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-04-27 17:00:18 +0100

    redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import Users from './user/pages/Users'
 
 const App = () => {
@@ -8,6 +8,7 @@ const App = () => {
       <Route exact path="/">
         <Users />
       </Route>
+      <Redirect to="/"/>
   </Router>
   )
 }

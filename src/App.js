97750ebb595251440c709b3470e4a3d37f2fcdd1
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-04-27 17:04:17 +0100

    /places/new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Router>
+      <Switch>
       <Route exact path="/">
         <Users />
       </Route>
+      <Route exact path="/places/new">
+        <NewPlace />
+      </Route>
       <Redirect to="/"/>
+      </Switch>
   </Router>
   )
 }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Users = () => {
-    return <h2>USers Work!</h2>
+    return <h2 > Users Works! < /h2>
 }
 
 export default Users
